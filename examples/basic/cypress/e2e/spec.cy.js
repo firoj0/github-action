9370fcba73bf628ae3ff80454102bb02f5ae4ec7
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2023-02-09 17:54:55 +0100

    Add deliberate error

--- examples/basic/cypress/e2e/spec.cy.js
+++ examples/basic/cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
 it('works', () => {
-  expect(42).to.equal(21 + 21)
+  expect(40).to.equal(21 + 21)
   cy.visit('https://example.cypress.io').then(() => {
     expect('hello').to.equal('hello')
   })
