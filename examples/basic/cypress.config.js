dbf98cbced9202daa71d9362745aeb762f776d36
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2023-10-28 19:25:35 +0200

    add after:run hook

--- examples/basic/cypress.config.js
+++ examples/basic/cypress.config.js
@@ -3,7 +3,23 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   fixturesFolder: false,
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      on('after:run', (results) => {
+        console.log('executing "after:run"');
+        console.log('whole object: ', results)
+        if (results) {
+          console.log('found results')
+          console.log('cypressVersion', results.cypressVersion)
+          console.log('totalDuration', results.totalDuration)
+          console.log('totalFailed', results.totalFailed)
+          console.log('totalPassed', results.totalPassed)
+          console.log('totalPending', results.totalPending)
+          console.log('totalSkipped', results.totalSkipped)
+          console.log('totalSuites', results.totalSuites)
+          console.log('totalTests', results.totalTests)
+        }
+      })
+    },
     supportFile: false,
   },
 })
